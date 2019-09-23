6d3b7e5a3a2ed7c10e1e702d1e803a5be76ca39d
ForrestS2017 <fsmith2017@gmail.com>
2019-09-23 15:31:08 -0400

    SL: Song Model
    + Created model for songs. Basic functionality for adding songs; will update later.

--- SongLib/src/Model/Song.java
+++ SongLib/src/Model/Song.java
@@ -1,10 +1,64 @@
 package Model;
 
+import java.util.UUID;
+
 /**
  * @author Forrest Smith
  * @author Jim Tang
  */
 
 public class Song {
-    // TODO
+    
+	private String album, artist, name, year, ID;
+	
+	public Song(String album, String artist, String name, String year)
+	{
+		this.album = album;
+		this.artist = artist;
+		this.name = name;
+		this.year = year;
+		
+		String id = UUID.randomUUID().toString();
+		ID = id.isEmpty() ? id : null;
+	}
+	
+	public boolean setName(String name) {
+		if (name.isBlank()) return false;
+		this.name = name;
+		return true;
+	}
+
+	public boolean setArtist(String artist) {
+		if (artist.isBlank()) return false;
+		this.artist = artist;
+		return true;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public String getArtist() {
+		return this.artist;
+	}
+
+	public String getAlbum() {
+		return this.album;
+	}
+
+	public String getYear() {
+		return this.year;
+	}
+
+	public String getID() {
+		return this.ID;
+	}
 }
