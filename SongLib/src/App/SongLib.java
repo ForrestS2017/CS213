31fdd65cd779dd41082fff11b9dcd5dc9229c88c
ForrestS2017 <fsmith2017@gmail.com>
2019-09-23 15:11:58 -0400

    SL: Completed Layout
    > Layout
    + Completed layout for app window
    + Created ID's for all required items
    
    > Main
    + Added routine loading of window
    + Added constants

--- SongLib/src/App/SongLib.java
+++ SongLib/src/App/SongLib.java
@@ -1,6 +1,9 @@
 package App;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -10,8 +13,20 @@ import javafx.stage.Stage;
 
 public class SongLib extends Application {
 
+	private final String LAYOUT_PATH = "/View/SongLib.fxml";
+	private final String APPLICATION_NAME = "Song Library";
+	private final boolean IS_RESIZEABLE = false;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+    	FXMLLoader loader = new FXMLLoader();
+    	loader.setLocation(getClass().getResource(LAYOUT_PATH));
+    	BorderPane root = (BorderPane)loader.load();
+    	Scene scene = new Scene(root);
+    	primaryStage.setScene(scene);
+    	primaryStage.setTitle(APPLICATION_NAME);
+    	primaryStage.setResizable(IS_RESIZEABLE);
+    	primaryStage.show();
         return;
     }
 
